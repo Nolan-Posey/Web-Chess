52a5b4f26cf7d6455e6d281ee746ce90ab924bee
Nolan Posey <nposey1@ewu.edu>
2024-06-14 13:26:26 -0700

    Chessboard now handles piece promotion.

--- web-chess/src/components/chess_components/MoveList.tsx
+++ web-chess/src/components/chess_components/MoveList.tsx
@@ -6,21 +6,28 @@ interface MoveListProps {
 }
 
 const MoveList = ({ moves }: MoveListProps) => {
-  
-    const formatMoves = (moves: string[]) => {
-        const formattedMoves = [];
-        for (let i = 0; i < moves.length; i += 2) {
-          formattedMoves.push(
-            `${moves[i]}${moves[i + 1] ? ` ${moves[i + 1]}` : ''}`
-          );
-        }
-        return formattedMoves;
-    };
+  const moveListRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (moveListRef.current) {
+      moveListRef.current.scrollTop = moveListRef.current.scrollHeight;
+    }
+  }, [moves]);
+
+  const formatMoves = (moves: string[]) => {
+    const formattedMoves = [];
+    for (let i = 0; i < moves.length; i += 2) {
+      formattedMoves.push(
+        `${moves[i]}${moves[i + 1] ? ` ${moves[i + 1]}` : ""}`
+      );
+    }
+    return formattedMoves;
+  };
 
   return (
     <div className="moveListContainer">
       <h2>Move History</h2>
-      <div className="moveList" >
+      <div className="moveList" ref={moveListRef}>
         <ol>
           {formatMoves(moves).map((move, index) => (
             <li key={index}>{move}</li>
--- web-chess/src/pages/ChessboardPage.tsx
+++ web-chess/src/pages/ChessboardPage.tsx
@@ -1,27 +1,56 @@
-import { useState } from 'react';
-import { Chess } from 'chess.js';
+import { useState } from "react";
+import { Chess, Square } from "chess.js";
 import { Chessboard } from "react-chessboard";
 import "./ChessboardPage.css";
-import MoveList from '../components/chess_components/MoveList';
+import MoveList from "../components/chess_components/MoveList";
+import { PromotionPieceOption } from "react-chessboard/dist/chessboard/types";
 
 function ChessboardPage() {
   const [game, setGame] = useState(new Chess());
   const [history, setHistory] = useState<string[]>([]);
 
-  const handlePieceDrop = (sourceSquare: string, targetSquare: string, piece: string) => {
-    const newGame = new Chess(game.fen());
+  const handlePieceDrop = (
+    sourceSquare: string,
+    targetSquare: string,
+    piece: string,
+    promotion?: string
+  ) => {
+    try {
+      const newGame = new Chess(game.fen()); // Create a new instance with the current position
+      const move = newGame.move({
+        from: sourceSquare,
+        to: targetSquare,
+        promotion: promotion || undefined,
+      });
 
-    const move = newGame.move({
-      from: sourceSquare,
-      to: targetSquare,
-      promotion: 'q'
-    });
+      if (move === null) {
+        console.log(`Invalid move: ${sourceSquare} to ${targetSquare}`);
+        return false; // Illegal move, do not update state
+      }
 
-    if(move === null) return false;
+      setGame(newGame);
+      setHistory([...history, move.san]);
+      return true; // Legal move, update state
+    } catch (error) {
+      console.error(
+        `Error processing move: ${sourceSquare} to ${targetSquare}`,
+        error
+      );
+      return false; // Handle the error and return false
+    }
+  };
 
-    setGame(newGame);
-    setHistory([...history, move.san]);
-    return true;
+  const handlePromotionPieceSelect = (
+    piece: PromotionPieceOption | undefined,
+    promoteFromSquare: Square | undefined,
+    promoteToSquare: Square | undefined
+  ): boolean => {
+    if (!promoteFromSquare || !promoteToSquare || !piece) {
+      return false;
+    }
+
+    const promotion = piece.toLowerCase()
+    return handlePieceDrop(promoteFromSquare, promoteToSquare, piece, promotion[1]);
   };
 
   const resetGame = () => {
@@ -32,11 +61,18 @@ function ChessboardPage() {
   return (
     <div className="chessboard-container">
       <div className="chessboard-wrapper">
-        <Chessboard boardWidth={700} position={game.fen()} onPieceDrop={handlePieceDrop}></Chessboard>
+        <Chessboard
+          boardWidth={700}
+          position={game.fen()}
+          onPieceDrop={handlePieceDrop}
+          onPromotionPieceSelect={handlePromotionPieceSelect}
+        ></Chessboard>
         <MoveList moves={history}></MoveList>
       </div>
       <div className="p-button">
-      <button onClick={resetGame} className='btn btn-success'>Reset Game</button>
+        <button onClick={resetGame} className="btn btn-success">
+          Reset Game
+        </button>
       </div>
     </div>
   );
