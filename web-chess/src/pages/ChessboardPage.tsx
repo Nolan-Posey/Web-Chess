96ec7fde7c693cfe19e908f215ed30dde64c3193
Nolan Posey <nposey1@ewu.edu>
2024-06-14 11:30:25 -0700

    Chessboard is now on the screen and centered.

--- web-chess/src/pages/ChessboardPage.tsx
+++ web-chess/src/pages/ChessboardPage.tsx
@@ -1,5 +1,15 @@
+import { Chessboard } from "react-chessboard";
+import "./ChessboardPage.css";
+
 function ChessboardPage() {
-  return <div>Chessboard Page</div>;
+
+  return (
+    <div className="chessboard-container">
+      <div className="chessboard-wrapper">
+        <Chessboard boardWidth={700}></Chessboard>
+      </div>
+    </div>
+  );
 }
 
 export default ChessboardPage;
