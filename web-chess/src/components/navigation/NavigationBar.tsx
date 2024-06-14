1ed9bf36e5b4508bf3eb15caa59b08bc66b6848e
Nolan Posey <nposey1@ewu.edu>
2024-06-14 10:00:22 -0700

    Styled all pagers to have same background color. Home page now has two buttons in the center of the screen.

--- web-chess/src/components/navigation/NavigationBar.tsx
+++ web-chess/src/components/navigation/NavigationBar.tsx
@@ -5,7 +5,7 @@ import {NavLink} from "react-router-dom";
 function NavigationBar() {
   return (
     <Navbar className="navbar-custom"  expand="lg">
-      <Navbar.Brand as={NavLink} to="/Home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand as={NavLink} to="/Home">Web-Chess</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
